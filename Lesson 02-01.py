fb03d9c7b7d73b7926c1f18c1908608916bc7b1b
IgorPugachev <igor0514@gmail.com>
2023-02-13 21:59:02 +1100

    Revert "Файлы практического задания 2"
    This reverts commit b81ce7a2866b5142efa74158e807cf5cd694021e.

--- Lesson 02-01.py
+++
@@ -1,4 +0,0 @@
-myList = [1, "some text", 2.056, True]
-
-for item in myList:
-    print(type(item))
--- Lesson 02-02.py
+++
@@ -1,8 +0,0 @@
-x = list(input("Введите какое-нибудь слово "))
-
-y = 0
-for i in range(int(len(x) / 2)):
-    x[y], x[y + 1] = x[y + 1], x[y]
-    y += 2
-
-print(x)
